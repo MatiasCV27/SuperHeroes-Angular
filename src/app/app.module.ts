60a2ad6f24cad5cc96a992af3d1bcccdbd20ac85
MatiasCV27 <matiascriollo57@gmail.com>
2023-08-22 19:14:21 -0500

    Creando las vistas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './views/home/home.component';
+import { ContactoComponent } from './views/contacto/contacto.component';
+import { SuperheroesComponent } from './views/mantenimiento/superheroes/superheroes.component';
+import { VillanosComponent } from './views/mantenimiento/villanos/villanos.component';
+import { UsuariosComponent } from './views/mantenimiento/usuarios/usuarios.component';
+import { EventosComponent } from './views/mantenimiento/eventos/eventos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactoComponent,
+    SuperheroesComponent,
+    VillanosComponent,
+    UsuariosComponent,
+    EventosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/views/galeria/villanos/villanos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-villanos',
+  templateUrl: './villanos.component.html',
+  styleUrls: ['./villanos.component.css']
+})
+export class VillanosComponent {
+
+}
